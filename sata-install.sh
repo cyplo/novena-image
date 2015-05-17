aaeddcfb851a2417a745e0756e4a1074053c7a08
Cyryl Plotnicki-Chudyk <cyplo@cyplo.net>
2015-05-17 20:44:26 +0200

    sata installer now hosts all depenedencies for mmc as well

--- sata-install.sh
+++ sata-install.sh
@@ -12,10 +12,23 @@ exec sudo ./novena-image.sh \
 	-t sata \
 	-s jessie \
 	-a pulseaudio-novena_1.1-r1_all.deb \
+	-a u-boot-novena_2014.10-novena-r2-rc17_armhf \
 	-a irqbalance-imx_0.56-1ubuntu4-rmk1_armhf.deb \
+	-a libdrm-armada2_2.0.2-1_armhf.deb \
+	-a libetnaviv_0.0.0-r11_armhf.deb \
+	-a novena-usb-hub_1.3-r1_armhf.deb \
+	-a linux-headers-novena_3.19-novena-r10_armhf.deb \
+	-a linux-image-novena_3.19-novena-r10_armhf.deb \
+	-a linux-firmware-image-novena_3.19-novena-r10_armhf.deb \
+	-a linux-libc-dev_3.19-novena-r10_armhf.deb \
 	-a novena-disable-ssp_1.1-1_armhf.deb \
-	-a u-boot-novena_2014.10-novena-r2-rc17_armhf.deb \
-	-a linux-image-3.19.0-00270-g3d69696-dbg_3.19-novena-r10_armhf.deb \
+	-a novena-eeprom_2.3-1_armhf.deb \
+	-a novena-eeprom-gui_1.2-r1_armhf.deb \
+	-a kosagi-repo_1.0-r1_all.deb \
+	-a novena-firstrun_1.6-r1_all.deb \
+	-a xorg-novena_1.5-r1_all.deb \
+	-a xserver-xorg-video-armada_0.0.1-r6_armhf.deb \
+	-a xserver-xorg-video-armada-etnaviv_0.0.1-r6_armhf.deb \
 	-l "sudo openssh-server ntp ntpdate dosfstools btrfs-tools \
 	    novena-eeprom xserver-xorg-video-modesetting task-gnome-desktop \
 	    hicolor-icon-theme gnome-icon-theme tango-icon-theme keychain \
