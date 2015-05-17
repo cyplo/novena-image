7195646fc1862f3fd94dc83e9f0572eef031f331
Cyryl Plotnicki-Chudyk <cyplo@cyplo.net>
2015-05-17 20:43:51 +0200

    script dowloading package dependencies added

---
+++ download_packages.sh
@@ -0,0 +1,20 @@
+#!/bin/sh
+wget -c http://repo.novena.io/repo/pool/main/p/pulseaudio-novena/pulseaudio-novena_1.1-r1_all.deb
+wget -c http://repo.novena.io/repo/pool/main/u/u-boot-novena/u-boot-novena_2014.10-novena-r2-rc17_armhf.deb
+wget -c http://repo.novena.io/repo/pool/main/i/irqbalance-imx/irqbalance-imx_0.56-1ubuntu4-rmk1_armhf.deb
+wget -c http://repo.novena.io/repo/pool/main/libd/libdrm-armada/libdrm-armada2_2.0.2-1_armhf.deb
+wget -c http://repo.novena.io/repo/pool/main/libe/libetnaviv/libetnaviv_0.0.0-r11_armhf.deb
+wget -c http://repo.novena.io/repo/pool/main/n/novena-usb-hub/novena-usb-hub_1.3-r1_armhf.deb
+wget -c http://repo.novena.io/repo/pool/main/l/linux-upstream/linux-headers-novena_3.19-novena-r10_armhf.deb
+wget -c http://repo.novena.io/repo/pool/main/l/linux-upstream/linux-image-novena_3.19-novena-r10_armhf.deb
+wget -c http://repo.novena.io/repo/pool/main/l/linux-upstream/linux-firmware-image-novena_3.19-novena-r10_armhf.deb
+wget -c http://repo.novena.io/repo/pool/main/l/linux-upstream/linux-libc-dev_3.19-novena-r10_armhf.deb
+wget -c http://repo.novena.io/repo/pool/main/n/novena-disable-ssp/novena-disable-ssp_1.1-1_armhf.deb
+wget -c http://repo.novena.io/repo/pool/main/n/novena-eeprom/novena-eeprom_2.3-1_armhf.deb
+wget -c http://repo.novena.io/repo/pool/main/n/novena-eeprom-gui/novena-eeprom-gui_1.2-r1_armhf.deb
+wget -c http://repo.novena.io/repo/pool/main/k/kosagi-repo/kosagi-repo_1.0-r1_all.deb
+wget -c http://repo.novena.io/repo/pool/main/n/novena-firstrun/novena-firstrun_1.6-r1_all.deb
+wget -c http://repo.novena.io/repo/pool/main/x/xorg-novena/xorg-novena_1.5-r1_all.deb
+wget -c http://repo.novena.io/repo/pool/main/x/xf86-video-armada/xserver-xorg-video-armada_0.0.1-r6_armhf.deb
+wget -c http://repo.novena.io/repo/pool/main/x/xf86-video-armada/xserver-xorg-video-armada-etnaviv_0.0.1-r6_armhf.deb
+
